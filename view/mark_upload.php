525a2aa48e72a07edc5b648f1b4781b819b28216
DESKTOP-OMQTFCT\vefetch <asudhakar@live.in>
2016-09-14 21:26:48 +0530

    created markupload

---
+++ view/mark_upload.php
@@ -0,0 +1,12 @@
+<?php include_once 'header.php'; ?>
+
+	<input type="textbox" class="form-control" name="name_of_exam" placeholder="Name of the Exam" required><br/>
+	<input type="textbox" class="form-control" name="class" placeholder="Class" required><br/>
+	<input type="textbox" class="form-control" name="section" placeholder="Section" required><br/>
+	<input type="file" class="form-control" name="markUpload" required><br/>
+	<p style="text-align:center">
+	<input type="submit" class="btn btn-info" style="font-size: 13px;"value="Upload File" name="submit">
+	</p>
+
+
+<?php include_once 'footer.php'; ?>
