70fe660428868b07b5cd71826129fa46951a1a2c
Rubak kumar <14cse11@gmail.com>
2016-10-04 18:53:33 +0530

    rubak works  on bulk sms

--- view/send_bulk_sms.php
+++ view/send_bulk_sms.php
@@ -6,10 +6,29 @@
 	$message = $_POST['message'];
 	$headers = $_SESSION['headers'];
 	print_r($headers);
-	str_replace(
-	   $headers,
-	    array("replace", "items"),
-    $message
-);
+	$count = count($_SESSION['bulk_data']);
+	echo "$count";
+
+	$header_count = count($headers);
+	echo $header_count;
+	unset($headers[$header_count-1]);
+	print_r($headers);
+	 for ($i=0; $i <3 ; $i++) { 
+		print_r($headers[$i]);
+	}
+	
+
+
+
+
+
+
+
+
+// 	str_replace(
+// 	$headers,
+// 	array("replace", "items"),
+// 	$message
+// );
 
  ?>
