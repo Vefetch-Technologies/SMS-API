14ca9d0b5cd8361afd28e38e48b34164cd1db638
DESKTOP-OMQTFCT\vefetch <afm447@gmail.com>
2016-10-07 14:31:06 +0530

    added notification in rest-api

--- view/bulk_sms.php
+++ view/bulk_sms.php
@@ -58,7 +58,7 @@
 						echo "<a href='view.php?file_name=".$value['file_name']."'>".$value['file_name']."</a>";
 						echo "</td>";
 						echo "<td>";
-						echo "<a href='delete.php?file_name=".$value['file_name']."'><button type='button' class='btn btn-danger' style='width:208px;'> Delete </button></a>";
+						echo "<a href='delete_file.php?file_name=".$value['file_name']."'><button type='button' class='btn btn-danger' style='width:208px;'> Delete </button></a>";
 						echo "</td>";
 					echo "</tr>";
 				}
--- view/rest_api.php
+++ view/rest_api.php
@@ -10,14 +10,21 @@
 				<input type="textbox" class="form-control" name="url" value="<?php echo $url.$remaining_url; ?>" >
 				<br>
 				<button class="btn btn-info col-md-3 pull-right" id="test" name="test">Test</button>
-			<script type="text/javascript">
-				$('body').on('click', '#test', function(){
-					url = '<?php echo $url.$remaining_url;?>';
-				$.ajax(url, { 'success': function(data) {
-					console.log(data);
-				}
-				});
-				})
-			</script>
+				<div id="response"></div>
+				<script type="text/javascript">
+					$('body').on('click', '#test', function(){
+						url = '<?php echo $url.$remaining_url;?>';
+						$.ajax(url, { 
+							'success': function(data) {
+								console.log(data);
+								if (data == "SMS Sent") {
+									document.getElementById('response').innerHTML = "<div class='alert alert-success col-md-6'><strong>Success!</strong>Message sent</div>"; 
+								} else{
+									document.getElementById('response').innerHTML = "<div class='alert alert-danger col-md-6'><strong>"+data+"</strong></div>"; 
+								} 
+							}
+						});
+					})
+				 </script>
 
 <?php include_once 'footer.php'; ?>
