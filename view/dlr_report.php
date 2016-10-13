74bd607a17ef7a591425852f4a03e19ee14db4c0
Rubak kumar <14cse11@gmail.com>
2016-10-13 12:00:15 +0530

    work on dlr_report

--- view/dlr_report.php
+++ view/dlr_report.php
@@ -1,3 +1,7 @@
 <?php include_once 'header.php'; ?>
+<div class="home_header">
+	<h1>Welcome <?php echo $user_details['user_name']; ?>, to Today's Report</h1>
+	<hr style="border-top: 1px solid #191616">
+</div>
 
 <?php include_once 'footer.php'; ?>
