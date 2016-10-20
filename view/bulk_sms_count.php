18fbbb302c9547d226942ed6ca9c9a8469f4a807
Rubak kumar <14cse11@gmail.com>
2016-10-20 15:44:34 +0530

    updated redirection

--- view/bulk_sms_count.php
+++ view/bulk_sms_count.php
@@ -59,7 +59,7 @@
 			}
 			document.getElementById('response').innerHTML = "<div class='alert alert-success'><strong>Please</strong>Wait a moment we are processing your messages</div>"; 
 			$('#send_bulk_sms').attr("disabled", true);
-			window.open("home.php");
+			window.open("http://sms2.vefetch.com/");
 			$.ajax({
 				type: "POST",
 				url: "send_bulk_sms.php",
